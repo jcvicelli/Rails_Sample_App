cc394a520897732a97cfd9e989e90f9076d84e39
jcvicelli <jcvicelli@gmail.com>
2015-12-02 20:31:24 -0200

    bugfix on test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
 
   end
 
@@ -24,7 +24,6 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
     assert_select "title", "About | #{@base_title}"
-
   end
 
 end
